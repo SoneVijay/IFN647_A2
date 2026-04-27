6f0b56f328fcc968850fa6ed977f59f6a66ee9ca
Bonn Michael Canoza <bmscanoza@Bonns-MacBook-Air.local>
2026-04-27 16:47:05 +1000

    testing2

--- A2.py
+++ A2.py
@@ -1,6 +1,7 @@
 # ============================================================
 # IFN647 – Assignment 2 | Complete Implementation
 # Student: BONN MICHAEL S. CANOZA | N12349208
+# Maganda
 # ============================================================
 
 import os
